915b8f3ae47d89b0e453fc2e87aabd648d21801b
qinxuanlong <1470681411@qq.com>
2023-09-06 23:16:03 +0800

    添加用户

---
+++ src/main/java/com/example/libraryservice/controller/request/UserPageRequest.java
@@ -0,0 +1,10 @@
+package com.example.libraryservice.controller.request;
+
+import lombok.Data;
+
+@Data
+public class UserPageRequest extends BaseRequest {
+    private String name;
+    private String phone;
+
+}
